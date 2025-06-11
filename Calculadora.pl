9c71644d96714c1f654fb8c37fccf1d16833acf4
Eduardo Fernandes dos Santos <eduardo.f.rocha@iesb.edu.br>
2025-06-11 08:53:27 -0300

    Uepaa

--- Calculadora.pl
+++ Calculadora.pl
@@ -1,7 +1,35 @@
+% Caso base: se for um número, a avaliação é ele mesmo.
 eval(X, X) :- number(X).
 
-% Regras recursivas para cada operacao:
-eval(X+Y, R) :- eval(X, A), eval(Y, B), R is A+B.
-eval(X-Y, R) :- eval(X, A), eval(Y, B), R is A-B.
-eval(X*Y, R) :- eval(X, A), eval(Y, B), R is A*B.
-eval(X/Y, R) :- eval(X, A), eval(Y, B), R is A/B.
+% Regras recursivas para cada operação aritmética:
+
+% Soma
+eval(X+Y, R) :-
+    eval(X, A),
+    eval(Y, B),
+    R is A + B.
+
+% Subtração
+eval(X-Y, R) :-
+    eval(X, A),
+    eval(Y, B),
+    R is A - B.
+
+% Multiplicação
+eval(X*Y, R) :-
+    eval(X, A),
+    eval(Y, B),
+    R is A * B.
+
+% Divisão segura
+eval(X/Y, R) :-
+    eval(Y, B),
+    B =\= 0,
+    eval(X, A),
+    R is A / B.
+
+% Caso de erro: divisão por zero
+eval(_/Y, _) :-
+    eval(Y, 0),
+    write('Erro: divisao por zero!'), nl,
+    fail.
